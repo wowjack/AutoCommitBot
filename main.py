65fbb04b505576ef232bd423a82f342e3a71688b
Ubuntu <ubuntu@ip-172-31-84-130.ec2.internal>
2022-06-14 02:30:34 +0000

    bot

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ if os.path.exists("./test.txt"):
 else:
     os.system("echo hello > test.txt")
 
-os.system("echo hello >> test.txt")
-
 os.system("git add -A")
 os.system("git commit -m 'bot'")
 os.system("git push")

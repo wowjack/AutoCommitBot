a41e50aa73d3ed99a63b0c0cac605cd28fbe913b
Ubuntu <ubuntu@ip-172-31-84-130.ec2.internal>
2022-06-14 02:49:22 +0000

    Stupid bot commit

---
+++ commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/ubuntu/repos/AutoCommitBot
+
+FILE=./test.txt
+if test -f "$FILE"; then
+    rm $FILE
+else
+    echo hello > test.txt
+fi
+
+git add -A
+git commit -m "Stupid bot commit"
+git push

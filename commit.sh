62719de97835e8af31edd14931da9abdfd47d441
Jack Kingham <wowknead@gmail.com>
2022-06-17 18:22:26 +0000

    06/17/22 Commit #1

--- commit.sh
+++ commit.sh
@@ -6,9 +6,9 @@ echo "Num commits: $(($LOOPS+1))" > log.txt
 for var in $(seq $(($LOOPS+1)))
 do
     echo -e "$(date)\t$(echo $RANDOM | md5sum)" >> log.txt
-    echo "Commiting"
     git add -A
-    git commit -m $RANDOM
-    git push
-    sleep 10
+    MESSAGE=$(echo "$(date +%D) Commit #$var")
+    git commit -m "$MESSAGE"
+    #git push
+    #sleep 10
 done
